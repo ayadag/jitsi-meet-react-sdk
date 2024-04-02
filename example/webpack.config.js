55b9d619fbf2fa134f4e7eb2c3f2d8bb4c6bb299
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-02 11:24:21 +0300

    dev

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         })
     ],
     devServer: {
+//////////////////////
+        compress: true,
+        public: 'store-client-nestroia1.c9users.io' // That solved it
+/////////////////////
         static: './',
         port: 5001
     }
