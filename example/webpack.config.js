14bf25479586dab9f1f0e594d461751048d0a66b
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-02 11:32:27 +0300

    dhc

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -31,10 +31,11 @@ module.exports = {
     ],
     devServer: {
 //////////////////////
-        compress: true,
-        public: 'store-client-nestroia1.c9users.io' // That solved it
+        //compress: true,
+        //public: 'store-client-nestroia1.c9users.io' // That solved it
 /////////////////////
-        //static: './',
-        //port: 5001
+        disableHostCheck: true,
+        static: './',
+        port: 5001
     }
 };
